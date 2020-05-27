6e91d73ce2f51834f0a24701ba287f0d420349e1
Erdem <rifaterdemsahin@gmail.com>
2020-05-27 20:54:32 +0100

    Update sendPortfolioToDiscord.py
    I have added a library @mehmetakifaksoy

--- LambdaFunctions/sendPortfolioToDiscord.py
+++ LambdaFunctions/sendPortfolioToDiscord.py
@@ -1,10 +1,44 @@
 #!python
 import json
+from urllib.request import urlopen, Request
+import urllib
+from bs4 import BeautifulSoup
 
 def sendPortfolioToDiscord(event, context):
-
+    url = 'https://www.etoro.com/people/rifaterdemsahin/portfolio'
+    headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 
+                      'AppleWebKit/537.11 (KHTML, like Gecko) '
+                      'Chrome/23.0.1271.64 Safari/537.11',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+        'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+        'Accept-Encoding': 'none',
+        'Accept-Language': 'en-US,en;q=0.8',
+        'Connection': 'keep-alive'}
     
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Start of the download!')
-    }
+    req = Request(url=url, headers=headers) 
+    html = urlopen(req).read() 
+
+    soup = BeautifulSoup(html)
+    
+    # kill all script and style elements
+    for script in soup(["script", "style"]):
+        script.extract()    # rip it out
+    
+    # get text
+    text = soup.get_text()
+    
+    # break into lines and remove leading and trailing space on each
+    lines = (line.strip() for line in text.splitlines())
+    # break multi-headlines into a line each
+    chunks = (phrase.strip() for line in lines for phrase in line.split("  "))
+    # drop blank lines
+    text = '\n'.join(chunk for chunk in chunks if chunk)
+    
+    print(text)
+
+    print(output)
+
+
+
+
+
