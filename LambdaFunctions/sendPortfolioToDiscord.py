b840a59540ac9ffa3de2ddf4d6eee0ddbfaa0f19
Erdem <rifaterdemsahin@gmail.com>
2020-05-27 20:40:47 +0100

    Update sendPortfolioToDiscord.py

--- LambdaFunctions/sendPortfolioToDiscord.py
+++ LambdaFunctions/sendPortfolioToDiscord.py
@@ -1,8 +1,10 @@
+#!python
 import json
 
 def sendPortfolioToDiscord(event, context):
-    # TODO implement
+
+    
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps('Start of the download!')
     }
